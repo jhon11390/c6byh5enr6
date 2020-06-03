de8486de3daf41b26b289d65fe59bc7d8ce0a586
jhon11390 <meljhon2@gmail.com>
2020-06-03 04:23:04 -0500

    Arregla controlador products y controlador api::v1::products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
     def index
-        @products = Product.all
-        render json: @products
+        render json: Product.all
     end
 end
