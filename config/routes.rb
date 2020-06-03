7cafbee4de686bc851ff6389cbf959300514c8f3
jhon11390 <meljhon2@gmail.com>
2020-06-03 04:29:01 -0500

    Modifica archivo rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :products
-  namespace 'api' do
-    namespace 'v1' do
-      resources :products
+  namespace :api , defaults: { format: :json } do
+    namespace :v1 do
+      resources :products, :only => [:index, :create, :update, :destroy]
     end
   end
 end
