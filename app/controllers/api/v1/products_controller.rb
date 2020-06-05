edee49e95cd9ce72f8ee0d2eb3ce9a934505399d
jhon11390 <meljhon2@gmail.com>
2020-06-05 14:36:39 -0500

    Crea los controladores update y destoy para api products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -14,6 +14,23 @@ class Api::V1::ProductsController < ApplicationController
         end
     end
 
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        render json: @product, status: :ok
+      else
+        render json: { errors: @product.errors }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      
+      head :no_content
+      
+    end
+  
     private
         def product_params
             params.require(:product).permit(:name, :price)
