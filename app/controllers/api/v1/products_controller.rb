a6efaf8f92c15b88d872b743e15e70e6c79e52b7
jhon11390 <meljhon2@gmail.com>
2020-06-05 11:04:37 -0500

    Crea productos en formato json

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::ProductsController < ApplicationController
+    protect_from_forgery with: :null_session
     def index
         render json: Product.all
     end
+
+    def create
+        @product = Product.new(product_params)
+    
+        if @product.save
+          render json: @product, status: :created
+        else
+          render json: { errors: @product.errors }, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def product_params
+            params.require(:product).permit(:name, :price)
+        end
 end
